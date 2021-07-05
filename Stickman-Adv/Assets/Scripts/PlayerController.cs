1f268a6c06cc5eec79ac12da1dca283096a48d45
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-07-05 20:38:55 -0300

    feat(player): implementation of jump script

--- Stickman-Adv/Assets/Scripts/PlayerController.cs
+++ Stickman-Adv/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private BoxCollider2D boxCollider2D;
-    private Rigidbody2D rigidbody2D;
+    private BoxCollider2D BoxCollider2D;
+    private Rigidbody2D Rigidbody2D;
     public float MovementSpeed;
     public float JumpSpeed;
 
     
     void Start()
     {
-        boxCollider2D = GetComponent<BoxCollider2D>();
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        BoxCollider2D = GetComponent<BoxCollider2D>();
+        Rigidbody2D = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        Rigidbody2D.velocity = new Vector2(horizontalMovement * MovementSpeed, Rigidbody2D.velocity.y);
+
+            Debug.Log(Input.GetKeyDown(KeyCode.Space));
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Rigidbody2D.velocity = new Vector2(Rigidbody2D.velocity.x, JumpSpeed);
+        }
         
     }
 
     private void FixedUpdate() {
-        float horizontalMovement = Input.GetAxisRaw("Horizontal");
-        rigidbody2D.velocity = new Vector2(horizontalMovement * MovementSpeed, rigidbody2D.velocity.y);
+
          
     }
 }
