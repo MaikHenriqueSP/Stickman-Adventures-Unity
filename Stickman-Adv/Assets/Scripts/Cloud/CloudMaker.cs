02f5614c212909749de8cddec01f30e052520638
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-07-18 14:33:56 -0300

    feat: creation of coroutine for cloud generation

--- Stickman-Adv/Assets/Scripts/Cloud/CloudMaker.cs
+++ Stickman-Adv/Assets/Scripts/Cloud/CloudMaker.cs
@@ -11,4 +11,29 @@ public class CloudMaker : MonoBehaviour
     public float spawnIntervalMax;    
     public GameObject[] clouds;
 
+    void Start()
+    {
+        StartCoroutine("GenerateClouds");
+    }
+
+    IEnumerator GenerateClouds()
+    {
+        for(;;)
+        {
+            float nextSpawnTime = generateSpawnTime();
+            SpawnCloud();
+            
+            yield return new WaitForSeconds(nextSpawnTime);
+        }
+    }
+
+    private void SpawnCloud()
+    {
+        
+    }
+
+    private float generateSpawnTime()
+    {
+        return UnityEngine.Random.Range(spawnIntervalMin, spawnIntervalMax);
+    }
 }
