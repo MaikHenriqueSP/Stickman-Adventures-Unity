aba0cff43ff0ddcac8119ab9ba88f66b068f3bf5
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-07-18 14:02:25 -0300

    feat: addition of base cloud generator structure
    - creation of cloud manager object with start and endpoints
    - import of the clouds sprites
    - creation of the base cloud generator script

---
+++ Stickman-Adv/Assets/Scripts/Cloud/CloudMaker.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMaker : MonoBehaviour
+{
+    private float cloudSpeed;
+    public GameObject startPoint;
+    public GameObject endPoint;
+    public float spawnIntervalMin;
+    public float spawnIntervalMax;    
+    public GameObject[] clouds;
+
+}
