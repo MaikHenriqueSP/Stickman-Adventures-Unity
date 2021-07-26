b9f3ae5a361c532796ba8a06e60da9a8bc222288
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-07-26 19:47:35 -0300

    feat: addition of triggering when the player go over the trigger checkpoint

--- Stickman-Adv/Assets/Scripts/Dialogue/DialogManager.cs
+++ Stickman-Adv/Assets/Scripts/Dialogue/DialogManager.cs
@@ -1,11 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogManager : MonoBehaviour
 {
+
+    public TMP_Text speakerName;
+    public TMP_Text dialogContent;
+
+
+    private Queue<Sentence> sentences;
     public void InitDialog(Dialogue dialog)
     {
-        Debug.Log("Dialog Init");
+        sentences = new Queue<Sentence>(dialog.sentences);
+    }
+
+    public void NextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            return;
+        }
+
+        StopAllCoroutines();
+        StartCoroutine(TypeSentences(sentences.Dequeue()));
+
+    }
+
+    private IEnumerator TypeSentences (Sentence sentence )
+    {
+        speakerName.text = sentence.SpeakerName;
+        dialogContent.text = "";
+
+        foreach (char character in sentence.phrase.ToCharArray())
+        {
+            dialogContent.text += character;
+            yield return null;
+        }
+
     }
 }
