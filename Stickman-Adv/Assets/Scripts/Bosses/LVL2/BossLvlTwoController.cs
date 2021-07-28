319eedc187bf9fc0ca2f29cdb05197824d8b1f83
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-07-28 06:57:02 -0300

    feat: addition of new sprites for ninja

--- Stickman-Adv/Assets/Scripts/Bosses/LVL2/BossLvlTwoController.cs
+++ Stickman-Adv/Assets/Scripts/Bosses/LVL2/BossLvlTwoController.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossLvlTwoController : MonoBehaviour
+public class BossLvlTwoController : EnemyController
 {
-    // Start is called before the first frame update
+    public Transform player;
+    public float TargetDistanceToPlayer;
+    public float RetreatDistance;
+    private Rigidbody2D rigidbody2D;
+    public float movementSpeed;
+
+    private Animator animator;
     void Start()
     {
-        
+        base.Start();
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    public void Move()
+    {
+        float playerXPosition = player.transform.position.x;
+        float currentXPosition = transform.position.x;
+        float distance = Mathf.Abs(playerXPosition - currentXPosition);
+        if (distance > TargetDistanceToPlayer)
+        {
+            animator.Play("Boss_Walking");
+            rigidbody2D.velocity = new Vector2(playerXPosition * movementSpeed, rigidbody2D.velocity.y);
+        } else if (distance < RetreatDistance)
+        {
+            animator.Play("Boss_Walking");
+            rigidbody2D.velocity = new Vector2(playerXPosition * movementSpeed * (-1), rigidbody2D.velocity.y);
+        } else 
+        {
+            //animator.Play("Boss_Idle");
+            rigidbody2D.velocity = Vector2.zero;
+        }
+    }
+
+    public override void ReceiveDamage(int damage)
+    {
+        if (!IsInvincible)
+        {
+            CurrentLifePoints -= damage;
+        }
     }
 }
