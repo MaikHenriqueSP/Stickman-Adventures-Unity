e68678dd9f03896a78d767501dbb18fb8f42e219
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-08-07 06:00:29 -0300

    refactor: move of commonly used method to the EnemyController

--- Stickman-Adv/Assets/Scripts/Bosses/LVL1/BossController.cs
+++ Stickman-Adv/Assets/Scripts/Bosses/LVL1/BossController.cs
@@ -34,32 +34,6 @@ public class BossController : EnemyController
         }
     }
 
-    private void updateHealthbar() {
-        float remainingLifePointsPercentage = CurrentLifePoints / (float) LifePoints;
-        BossHealthBar.BossHealthbarSingleton.SetValue(remainingLifePointsPercentage);
-    }
-
-    public float HalfBossLife()
-    {
-        return LifePoints / 2;
-    }
-
-    public void BecomeUnbeatable()
-    {
-        IsInvincible = true;
-    }
-
-    public void BecomeBeatable()
-    {
-        IsInvincible = false;
-    }
-
-    public bool IsDead()
-    {
-        return CurrentLifePoints <= 0;
-    }
-
-
     public void ShakeCamera()
     {
         cameraAnimator.SetTrigger("Earthquake");
--- Stickman-Adv/Assets/Scripts/EnemyController.cs
+++ Stickman-Adv/Assets/Scripts/EnemyController.cs
@@ -126,5 +126,30 @@ public class EnemyController : MonoBehaviour
         return isTurnedLeft;
     }
 
+    private void updateHealthbar() {
+        float remainingLifePointsPercentage = CurrentLifePoints / (float) LifePoints;
+        BossHealthBar.BossHealthbarSingleton.SetValue(remainingLifePointsPercentage);
+    }
+
+    public float HalfBossLife()
+    {
+        return LifePoints / 2;
+    }
+
+    public void BecomeUnbeatable()
+    {
+        IsInvincible = true;
+    }
+
+    public void BecomeBeatable()
+    {
+        IsInvincible = false;
+    }
+
+    public bool IsDead()
+    {
+        return CurrentLifePoints <= 0;
+    }
+
 
 }
