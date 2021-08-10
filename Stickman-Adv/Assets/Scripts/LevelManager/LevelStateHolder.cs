426d244358ef7ae716743c11a3f01c6a1bafd813
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-08-10 18:42:42 -0300

    feat: addition of the third level unlocked screen

--- Stickman-Adv/Assets/Scripts/LevelManager/LevelStateHolder.cs
+++ Stickman-Adv/Assets/Scripts/LevelManager/LevelStateHolder.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LevelStateHolder : MonoBehaviour
 {
     public static int CurrentLevel {get; set;}
+    public static readonly int AvailableLevels = 3;
     public static void GoNextLevel()
     {
         CurrentLevel++;
